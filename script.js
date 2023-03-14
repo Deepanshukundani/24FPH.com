f2907e8a69a1e7931f4e14b98b0832d5996fa14e
Deepanshu Kundani <dkdeepanshu7@gmail.com>
2023-03-14 17:02:22 +0530

    24FPH

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Film Maker", "Cimematographer", "Diractor", "Editor", "D.O.P."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
